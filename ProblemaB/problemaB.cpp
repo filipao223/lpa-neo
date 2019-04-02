09e005040beeb8b7302377b66e6848ece325a89a
filipao223 <jpmontenegro67@gmail.com>
2019-04-02 16:44:00 +0100

    150 pontos, ainda sem função de validaçao

--- ProblemaB/problemaB.cpp
+++ ProblemaB/problemaB.cpp
@@ -4,7 +4,6 @@
 #include <math.h>
 
 #define MAX_TEMP 2024
-#define MAX_DEADLINE 100001
 
 typedef struct Event{
     int deadline;
@@ -13,10 +12,18 @@ typedef struct Event{
 } Event;
 
 int max(int a, int b){ return a>b?a:b; }
+int min(int a, int b){ return a>b?b:a; }
 
-/**/
-void print_DP(int DP[][MAX_DEADLINE], int num_events, int max_deadline, int width){
-    /*-------FOR NICE PRINTING ONLY--------*/
+
+
+
+
+/********************************************************************
+ * Function that prints resulting sum matrix, DP. For debugging only.
+ *
+ ********************************************************************/
+void print_DP(int **DP, int num_events, int max_deadline, int width){
+    /*-----------FOR NICE PRINTING ONLY----------*/
     //Print deadline numbers
     for (int i=0; i<width+3; i++) printf(" ");
     for (int i=0; i<=max_deadline; i++){
@@ -41,7 +48,14 @@ void print_DP(int DP[][MAX_DEADLINE], int num_events, int max_deadline, int widt
     printf("\n");
 }
 
-/*Simple function that prints received input. For debug purposes only.*/
+
+
+
+
+/**********************************************************************
+ * Simple function that prints received input. For debug purposes only.
+ * 
+ **********************************************************************/
 void print_input(Event event_list[], int num_events){
     printf("Received %d events: \n", num_events);
     for (int i=0; i<num_events; i++){
@@ -51,79 +65,68 @@ void print_input(Event event_list[], int num_events){
     printf("Done\n");
 }
 
+
+
+
+
+/*********************************************************
+ * Comparison function for qsort, smaller deadlines first.
+ *
+ *********************************************************/
 int comparison(const void *a, const void *b){
     Event *eventA = (Event *)a;
     Event *eventB = (Event *)b;
 
-    if (eventA->deadline < eventB->deadline) return -1; //A goes first
-    else if(eventA->deadline > eventB->deadline) return 1; //B goes first
-    else{ //Equal deadlines
-        if (eventA->duration > eventB->duration) return 1; //B goes first
-        else return -1; //A goes first
-    }
+    return eventA->deadline - eventB->deadline;
 }
 
-/*
-    Ideia 1:
-        1-Ordenar os eventos por deadline;
-        2-Fazer uma arvore como no ProblemaA do treino2
-            (Escolher a maior soma possivel)
-            Ver: https://pastebin.com/pbC8zins
-        3-Cada nivel tem o mesmo deadline
-        4-Escolher o maior profit de cada nivel e passar para o nivel
-            equivalente a nivel_atual + duration, p .ex:
-            2, tem deadline 2, o evento com maior profit tem duraçao
-            5, entao passa para o nivel 7, deadline 7, ...
-            (Esta parte é duvidosa)
-        5- ???
-        6- Profit
-    
-    Ideia 2:
-        1-Ordenar os eventos por deadline;
-        2-Não fazer uma arvore, mas usar o array dos eventos
-        3-Usar outro array para guardar somas
-        4-Usar um contador que guarde a duraçao atual e lucro atual
-        5-Entrar na funçao recursivamente, aumentando o index
-        6-Se duraçao for superior a deadline, proseguir
-        7-Se nao, aumentar a duraçao e lucro
-        8-Se chegar ao fim, voltar recursivamente e guardar soma em sum_array
 
-    Ideia 3 (best ideia) (USAR ESTA):
-        Tabela do stor
-        Deadline:       1 2 3 4 5 6 7 8 9     Lista eventos: Duraçao
-      Numero Eventos
-            |
-            0           T F F F F F F F F       Evento 1: 3
-            1           T F T T T F F F F       Evento 2: 2
-            2           T ...                   Evento 3: 4
-            3           T ...                   Evento 4: 6
-            4           T ...                   Evento 5: 2
 
-        Em vez de T ou F, usar o profit de cada evento, como no exemplo Knapsack
-        Ver: https://pastebin.com/i4aeQnmg
 
-*/
+
+
+/*****************************************************************************************************
+ * Main problem function
+ * Adapted from Knapsack problem bottom up algorithm: https://pastebin.com/i4aeQnmg.
+ * 
+ * Parameters:
+ *      event_list[]: List of received events, struct Event, containing deadline, duration and profit;
+ *      num_events: Number of received events, plus one in order to count from 0;
+ *      max_deadline: Maximum deadline found in received events, plus one in order to count from 0;
+ *      DP[][]: Sum matrix, to save subproblems answers.
+ * 
+ * Returns:
+ *      Single integer, representing largest possible profit.
+ *****************************************************************************************************/
 int problemB(Event event_list[], int num_events, int max_deadline, int **DP){
-    for (int i=0; i<=num_events; i++){ //Inicia primeira coluna de DP a 0
+    for (int i=0; i<=num_events; i++){ //Initializes DP's first column to 0
         DP[i][0] = 0;
     }
 
-    for (int j=1; j<=max_deadline; j++){ //Inicia primeira linha (a contar da segunda coluna) de DP a 0
+    for (int j=1; j<=max_deadline; j++){ //Initializes DP's first row (counting from second column) to 0
         DP[0][j] = 0;
     }
 
     for (int i=1; i<=num_events; i++){
         for (int j=1; j<=max_deadline; j++){
-            if (event_list[i-1].deadline>j) 
+            if (event_list[i-1].duration > min(j, event_list[i-1].deadline)) //If duration is bigger than the smallest deadline, then event can't fit
                 DP[i][j] = DP[i-1][j];
             else 
-                DP[i][j] = max(DP[i-1][j], DP[i-1][j-event_list[i-1].duration] + event_list[i-1].profit);
+                DP[i][j] = max(DP[i-1][j], DP[i-1][min(j, event_list[i-1].deadline) - event_list[i-1].duration] + event_list[i-1].profit);
         }
     }
 
     return DP[num_events][max_deadline];
 }
 
+
+
+
+
+/*****************************************************************
+ * Main function. Receives input and fills event list with events
+ * 
+ *****************************************************************/
 int main(){
 
     char temp[MAX_TEMP];
@@ -148,27 +151,24 @@ int main(){
             event_list[i] = event;
         }
 
-        /*Sort by deadline*/
+        /*Sort by deadline (smallest first)*/
         qsort(event_list, num_events, sizeof(Event), comparison);
 
         /*Get largest deadline*/
         int max_deadline = event_list[num_events-1].deadline;
 
-        /*Test received input*/
+        /*Test received input (for debugging only)*/
         //print_input(event_list, num_events);
 
-        /*Array that sum results*/
-        //int DP[num_events+1][MAX_DEADLINE]; //+1 to include zero events and zero deadline at beggining. See lpa_15_mar.txt line 32
-        //int (*DP)[MAX_DEADLINE] = malloc((num_events+1) * sizeof *DP);
+        /*Allocate dynamic programming subproblem result sum matrix*/
         int **DP = (int**)malloc((num_events+1) * sizeof *DP);
         int *data = (int*)malloc((num_events+1) * (max_deadline+1) * sizeof *data);
         for (int i=0; i<num_events+1; i++, data+=max_deadline+1) DP[i] = data;
 
         /*Main problem function*/
-        int rc = problemB(event_list, num_events, max_deadline, DP);
-        printf("%d\n", rc);
+        printf("%d\n", problemB(event_list, num_events, max_deadline, DP));
 
-        /*Print DP*/
+        /*Print DP (for debugging only)*/
         /*printf("\n");
         int maxval = DP[num_events][max_deadline]; //Adjust for different sized numbers in output
         int width = round(1+log(maxval)/log(10)); //Adjust for different sized numbers in output
